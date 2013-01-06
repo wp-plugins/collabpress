fca94e5096604184198f14fa751e3108e63c7129
ericlewis <ericlewis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-06 16:00:29 +0000

    1.2.3 version bump
    git-svn-id: https://plugins.svn.wordpress.org/collabpress/trunk@648765 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cp-loader.php
+++ cp-loader.php
@@ -3,7 +3,7 @@
 Plugin Name: CollabPress
 Plugin URI: http://collabpress.org/
 Description: A Project Management Plugin for WordPress
-Version: 1.2.2
+Version: 1.2.3
 Author: WebDevStudios.com
 Author URI: http://webdevstudios.com/
 License: GPLv2
